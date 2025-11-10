69aa09ba8515eef70de45bd5ba1749cf4447a951
ArcidesFerrao <cidesferrao@gmail.com>
2025-11-10 14:03:33 +0200

    prisma generate on production

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,15 @@
-// import Image from "next/image";
-
 import { Banner } from "@/components/Banner";
 import ItemsList from "@/components/ItemsList";
-// import { ProductModal } from "@/components/ProductModal";
 import { Slider } from "@/components/Slider";
 
 export default function Home() {
   return (
     <>
       <Slider />
-      <Banner />
-      {/* Product */}
-      <ItemsList />
 
-      {/*Product Modal1 */}
-      {/* <ProductModal /> */}
+      <Banner />
+
+      <ItemsList />
     </>
   );
 }
