38b0129838f61d0b5cafda6e6614d97eaf7e14da
ArcidesFerrao <cidesferrao@gmail.com>
2025-11-11 22:22:44 +0200

    footer icons + ref links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,13 +33,16 @@ export const Footer = () => {
               <h4 className="stext-301 cl0 p-b-10">Help</h4>
               <ul>
                 <li className="p-b-10">
-                  <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
+                  <a
+                    href="/contacto"
+                    className="stext-107 cl7 hov-cl1 trans-04"
+                  >
                     Devoluçao
                   </a>
                 </li>
 
                 <li className="p-b-10">
-                  <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
+                  <a href="/termos" className="stext-107 cl7 hov-cl1 trans-04">
                     FAQs
                   </a>
                 </li>
@@ -47,47 +50,29 @@ export const Footer = () => {
             </div>
             <div className="col-sm-6 col-lg-3 p-b-50  ">
               <h4 className="stext-301 cl0 p-b-10">Contacte-nos</h4>
-              <div className="p-t-2">
+              <div>
+                <a href="#" className="fs-18 cl7 hov-cl1 trans-04 m-r-16">
+                  <i className="fa fa-whatsapp" />
+                </a>
                 <a href="#" className="fs-18 cl7 hov-cl1 trans-04 m-r-16">
                   <i className="fa fa-facebook" />
                 </a>
                 <a href="#" className="fs-18 cl7 hov-cl1 trans-04 m-r-16">
                   <i className="fa fa-instagram" />
                 </a>
-                <a href="#" className="fs-18 cl7 hov-cl1 trans-04 m-r-16">
-                  <i className="fa fa-pinterest-p" />
-                </a>
               </div>
             </div>
-            {/* <div className="col-sm-6 col-lg-3 p-b-50 h-fit">
-              <h4 className="stext-301 cl0 p-b-30">Newsletter</h4>
-              <form>
-                <div className="wrap-input1 w-full p-b-4">
-                  <input
-                    className="input1 bg-none plh1 stext-107 cl7"
-                    type="text"
-                    name="email"
-                    placeholder="email@example.com"
-                  />
-                  <div className="focus-input1 trans-04" />
-                </div>
-                <div className="p-t-18">
-                  <button className="flex-c-m stext-101 cl0 size-103 bg1 bor1 hov-btn2 p-lr-15 trans-04">
-                    Subscribe
-                  </button>
-                </div>
-              </form>
-            </div> */}
           </div>
           <div className="p-t-40">
             <p className="stext-107 cl6 txt-center">
-              {/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}
-              Copyright © All rights reserved | Made with{" "}
-              <i className="fa fa-heart-o" aria-hidden="true" /> by{" "}
-              <a href="https://colorlib.com" target="_blank">
-                Colorlib
+              Copyright © All rights reserved | Made by{" "}
+              <a
+                href="https://portfolio-arcidesferraos-projects.vercel.app/"
+                target="_blank"
+              >
+                Arcides Ferrao
               </a>{" "}
-              &amp; distributed by{" "}
+              &amp; distributed theme by{" "}
               <a href="https://themewagon.com" target="_blank">
                 ThemeWagon
               </a>
