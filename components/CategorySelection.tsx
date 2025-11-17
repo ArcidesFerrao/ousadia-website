0378d0791122d626a11a56e6130ce11475243cbe
ArcidesFerrao <cidesferrao@gmail.com>
2025-11-17 09:52:02 +0200

    product modal on selected category

--- components/CategorySelection.tsx
+++ components/CategorySelection.tsx
@@ -41,24 +41,30 @@ export function CategorySelected({
 }: {
   category: CategoryWithProducts;
 }) {
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
   return (
     <>
       <div className="p-b-10" id={category.id}>
         <h3 className="ltext-103 cl5">{category.name}</h3>
       </div>
-      <div className="row isotope-grid">
-        <div className="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item ">
-          {category.products.length !== 0 ? (
-            category.products.map((product) => (
+      <div className="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item ">
+        {category.products.length !== 0 ? (
+          category.products.map((product) => (
+            <div
+              key={product.id}
+              onClick={() => setSelectedProduct(product)}
+              className="row isotope-grid"
+            >
               <ProductCard key={product.id} product={product} />
-            ))
-          ) : (
-            <p className="text-nowrap">
-              Nenhum produto encontrado nesta categoria.
-            </p>
-          )}
-        </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-nowrap">
+            Nenhum produto encontrado nesta categoria.
+          </p>
+        )}
       </div>
+      {selectedProduct && <ProductModal product={selectedProduct} />}
     </>
   );
 }
